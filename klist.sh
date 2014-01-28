a0cbcf465fb5dc1bb37fc63f17a3082a1028db28
Toni Mueller <dev@tonimueller.org>
2014-01-28 18:21:30 +0100

    on the list of keys, print the current year

--- klist.sh
+++ klist.sh
@@ -27,7 +27,7 @@ done
 
 # Print each key neatly into the keylist.
 (
-    cat $basedir/klist.head
+    sh $basedir/klist.head
 
     gpg --homedir $gpghome -q --no-options --fingerprint --list-key |
 	grep -v '^sub' |
